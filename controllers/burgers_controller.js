ab0b1acf66f849a0f3a40c26fbf561909287ec4b
Bobby Angus <Bobby Angus>
2018-01-07 18:49:19 -0500

    second push

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,71 +1,44 @@
 var express = require("express");
 
-var router = express.router();
+var router = express.Router();
 
-var db = require("../models");
+var db = require("../models/");
 
-router.get("/", function(req,res){
-	res.redirect("/burgers");
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
 });
 
-router.get("/burgers", function(req,res){
-	db.Burger.findAll({
-		include: [db.Customer],
-		order: [
-		["burger_name", "ASC"]
-		]
-	})
-})
-
-.then(function(dbBruger){
-	var hbsObject = {
-		burger: dbBurger
-	};
-	return res.render("index", hbsObject);
+router.get("/burgers", function(req, res) {
+  db.Burger.findAll()
+    .then(function(dbBurger) {
+      console.log(dbBurger);
+      var hbsObject = { burger: dbBurger };
+      return res.render("index", hbsObject);
+    });
 });
 
-router.post("/burgers/create", function(req,res){
-	db.Burger.create({
-		burger_name: req.body.burger_name
-	})
-	.then(function(dbBurger){
-		console.log(dbBurger);
-		res.redirect("/");
-	});
+router.post("/burgers/create", function(req, res) {
+  db.Burger.create({
+    burger_name: req.body.burger_name
+  })
+  .then(function(dbBurger) {
+    console.log(dbBurger);
+    res.redirect("/");
+  });
 });
 
-router.put("/burgers/update", function(req,res){
-	if(req.body.customer){
-		db.Customer.create({
-			customer: req.body.customer,
-			BurgerId: req.body.burger_id
-		})
-		.then(function(dbCustomer){
-			return db.Burger.update({
-				devoured:true
-			},{
-				where:{
-					id: req.body.burger_id
-				}
-			}
-			});
-		})
-		.then(function(dbBurger){
-			res.json("/");
-		});
-	}
-	else{
-		db.Burger.update({
-			devoured:true
-		},{
-			where:{
-				id: req.body.burger_id
-			}
-		}
-		})
-		.then(function(dbBurger){
-			res.josn("/");
-		});
-	}
+router.put("/burgers/update/:id", function(req, res) {
+  db.Burger.update({
+    devoured: true
+  },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(function(dbBurger) {
+    res.json("/");
+  });
 });
-module.exports=router;
+
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,19 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
-	var Burger = sequelize.define("Burger", {
-	burger_name : {
-	type: DataTypes.STRING,
-	allowNull: fa;se
-	},
-	devoured: {
-	type: DataTypes.BOOLEAN,
-	defaultValue: false
-	}
-	}, {
-	classMethods: {
-	associate: function(models){
-	Burger.hasone(models.Customer);
-	}
-	}
-	});
-	return Burger;
+  var Burger = sequelize.define("Burger", {
+    burger_name: DataTypes.STRING,
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+      }
+    }
+  });
+  return Burger;
 };
